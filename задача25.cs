1486b4fa6ec15213212c004ad505732d02d96ff1
timmashkov <106866033+timmashkov@users.noreply.github.com>
2022-10-01 09:50:25 +0300

    Create задача25.cs

---
+++ задача25.cs
@@ -0,0 +1,11 @@
+Console.WriteLine("введите число A");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите число B");
+int b = Convert.ToInt32(Console.ReadLine());
+int step = a;
+
+for (int i = 1; i < b; i++)
+{
+step = step * a;
+}
+Console.WriteLine("A в степени B равно: " + step);
